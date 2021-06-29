0c1135d3d5a1f3c3f83ab4076952a4615505a6de
basil george <baezl4life@gmail.com>
2021-06-29 17:05:55 +0300

    adding navbar

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,72 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import "./Homepage.scss";
+const Homepage = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <img
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        accusamus, saepe impedit deleniti, accusantium totam fuga voluptates,
+        excepturi tempore ducimus atque aliquid ipsam culpa sunt nobis molestias
+        vero rerum mollitia consequatur voluptas consectetur architecto.
+        Explicabo enim ratione alias porro excepturi cumque cum. Necessitatibus
+        aspernatur autem quo tempore illum eveniet architecto dicta? Quasi
+        accusamus rerum odit aperiam sunt, reprehenderit sit ducimus, distinctio
+        officiis amet ad eius qui minus, quaerat nisi voluptate culpa! Magni
+        labore esse aut eius, voluptatem, ratione laborum deserunt suscipit nam
+        accusamus ut at ad animi totam eveniet quibusdam quisquam fugiat dolorem
+        eos, ex cupiditate aperiam rem beatae voluptas! Sapiente dolor corrupti,
+        molestiae maxime nostrum quis quisquam doloribus consectetur eum iste
+        recusandae quod dolorum molestias, nulla necessitatibus a inventore
+        similique tempora excepturi nisi incidunt ad sit! Consequuntur autem
+        odio quos voluptates nisi esse sequi reprehenderit id praesentium unde
+        officiis recusandae, veritatis incidunt, quia dignissimos minima. Atque
+        quia laudantium quo ipsum eius aliquam quam exercitationem dolor, sint
+        error officiis distinctio? Magni laudantium at dolorum similique eum sed
+        expedita dicta corporis quisquam consequuntur, in totam ad ut delectus
+        cupiditate saepe. Accusantium ut delectus nesciunt aspernatur
+        reprehenderit recusandae rerum quisquam nisi commodi earum, minus porro
+        repudiandae temporibus necessitatibus incidunt, nam sunt fuga eveniet
+        corporis, ratione id facere! Ut eveniet ullam nobis quis veniam aliquam,
+        deserunt deleniti hic voluptates non iste. Ex vitae ab fuga animi
+        dolores. Illum minus dolorum a magnam. Explicabo modi voluptatem quam
+        amet optio non omnis provident incidunt. Enim illo ipsum et nulla ea
+        iste culpa iure consequatur possimus non rem ipsa optio reprehenderit
+        minima voluptate, minus commodi? Libero omnis quis magni atque
+        accusantium! Fugiat blanditiis dolores neque sed corrupti nemo obcaecati
+        quam illo ducimus minus explicabo error doloribus autem in similique
+        unde nihil qui at, voluptatum nisi, aliquam aspernatur aliquid itaque.
+        Dolorem beatae officiis dolor vitae unde neque nisi earum dicta
+        consequatur minus iure sunt magnam nam error sint temporibus, inventore
+        ab, ut, dolore omnis! Repellat aspernatur inventore repudiandae
+        laboriosam est at, quam numquam fugit veritatis consequatur et culpa
+        dolores voluptatem similique obcaecati, iusto qui reiciendis nam illo.
+        Voluptatem distinctio deleniti eveniet tempore libero, quas
+        necessitatibus nisi accusantium consequuntur vitae repudiandae facilis
+        reiciendis qui sit exercitationem culpa blanditiis porro recusandae?
+        Nesciunt, quo consequuntur ut assumenda exercitationem voluptatum
+        ratione molestiae a amet. Aut quod aperiam officia minima illum. A dolor
+        aperiam debitis, voluptatem quasi minus consequuntur maxime officiis
+        natus consequatur, eos iste, optio omnis quod odit? Exercitationem magni
+        repellat voluptatibus qui veniam voluptates? Incidunt, ipsum, sapiente a
+        eaque est in dignissimos, sed assumenda saepe reprehenderit unde magni
+        ipsam sit! Similique nihil omnis fugit, odit tempore maxime itaque sed
+        excepturi, nemo assumenda esse harum sequi unde error corporis? Ad at
+        harum perferendis accusamus ipsam vel iusto nemo tempora odio, libero,
+        quidem sapiente saepe quibusdam facilis facere voluptatum architecto
+        numquam fugiat doloribus labore cumque velit blanditiis qui?
+        Perspiciatis voluptates labore neque ut facilis voluptate in cupiditate
+        quisquam natus repudiandae culpa nulla pariatur est aliquid excepturi,
+        quos, error ullam velit illo consequuntur esse necessitatibus. Saepe
+        veniam facere asperiores reiciendis accusamus eos vero?
+      </p>
+    </div>
+  );
+};
+
+export default Homepage;
